da0f721ca746417fb3a15c1fabc0f47b7a967765
MERT CAN DÜLDÜL <mertcanduldul@gmail.com>
2022-04-20 01:45:11 +0300

    removed fantastic component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,14 +1,14 @@
 import { Feature } from '~/component/feature';
 import Header from '~/component/header';
 import Navigation from '~/component/navigation';
-import Fantastic from '~/component/fantastic';
+// import Fantastic from '~/component/fantastic';
 export default function Index() {
   return (
     <div>
       <Navigation />
       <Header />
       <Feature />
-      <Fantastic/>
+      {/* <Fantastic/> */}
     </div>
   );
 }
