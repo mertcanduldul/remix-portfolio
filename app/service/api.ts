49676fa0c5cdc684dd38fb91480a3e8d33abeecb
MERT CAN DÜLDÜL <mertcanduldul@gmail.com>
2022-07-12 13:41:43 +0300

    Update api.ts

--- app/service/api.ts
+++ app/service/api.ts
@@ -1,6 +1,6 @@
 import { IRepository } from "~/interface/IRepository";
 
-const BASE_URL = "https://api.github.com/users/mduldul/repos";
+const BASE_URL = "https://api.github.com/users/mertcanduldul/repos";
 
 export const getRepository: () => Promise<IRepository[]> = async () => {
     const response = await fetch(BASE_URL)
