947603c60f22e9831a0fd4f0c8076435f5707138
Mert <mertcanduldul@gmail.com>
2022-10-24 01:53:50 +0300

    update text

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -1,9 +1,9 @@
-import { AiFillInstagram, AiFillLinkedin, AiOutlineGithub } from 'react-icons/ai'
+import { AiFillInstagram, AiFillLinkedin, AiFillMediumSquare, AiOutlineGithub } from 'react-icons/ai'
 
 export default function Header() {
     return (
         <div className="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 bg-yellow-50 ">
-            <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
+            <div className="inset-y-0 top-0 right-0 z-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-6/12 lg:max-w-full lg:absolute xl:px-0">
                 <svg
                     className="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block"
                     viewBox="0 0 100 100"
@@ -20,15 +20,15 @@ export default function Header() {
             </div>
             <div className="relative flex flex-col items-start w-full max-w-xl px-4 mx-auto md:px-0 lg:px-8 lg:max-w-screen-xl">
                 <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
-                    <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+                    <p className="inline-block px-3 py-px mb-4 text-s font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
                         Mert Can Düldül
                     </p>
                     <h2 className="mb-5 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                        Software Engineer
+                        Backend Developer
                         <br className="hidden md:block" />
                         {' '}at{' '}
-                        <a className="inline-block text-blue-400 hover:text-orange-400 transition-all duration-300" href="https://isnet.net.tr/">
-                            isnet.net.tr
+                        <a className="inline-block text-blue-400 hover:text-orange-400 transition-all duration-300" href="https://www.unluco.com/">
+                            UNLU&CO
                         </a>
                     </h2>
                     <p className="pr-5 mb-5 text-base text-gray-700 md:text-lg">
@@ -43,6 +43,9 @@ export default function Header() {
                         </a>
                         <a href="https://www.instagram.com/m.duldul/" target="_blank" rel="noreferrer">
                             <AiFillInstagram className="h-12 w-12" />
+                        </a>
+                        <a href="https://mertcanduldul.medium.com" target="_blank" rel="noreferrer">
+                            <AiFillMediumSquare className="h-12 w-12" />
                         </a>
                     </div>
 
--- app/component/navigation.tsx
+++ app/component/navigation.tsx
@@ -15,7 +15,7 @@ export default function Navigation() {
                     <NavLink prefetch="intent" className="mr-5 hover:text-gray-900" to="/project">My Project's</NavLink>
                     <NavLink className="mr-5 hover:text-gray-900" to="/">Setup</NavLink>
                 </nav>
-                <a href="https://github.com/mduldul" target="_blank" rel="noreferrer" className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Jump to My Github Profile
+                <a href="https://github.com/mertcanduldul" target="_blank" rel="noreferrer" className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Jump to My Github Profile
                     <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
